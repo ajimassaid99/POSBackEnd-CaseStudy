128718cdcb465971c240e1a043c692bb3890a5da
Muhammad Aji Massaid <rmassaid99@gmail.com>
2023-07-10 11:39:17 +0700

    finisin

--- POS/database/index.js
+++ POS/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const{dbHost,dbPass,dbName,dbPort,dbUser}= require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+mongoose.connect(`mongodb+srv://rmassaid99:XYsppT49ReEXpCfu@cluster0.egisxkz.mongodb.net/?retryWrites=true&w=majority`);
 
 const db=mongoose.connection;
 
